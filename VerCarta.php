39120a0b407abc99c8bd90e26ee33bfc73bb347f
mcanela30 <60703913+mcanela30@users.noreply.github.com>
2020-12-05 18:18:18 +0100

    arreglar el ordenexito

--- VerCarta.php
+++ VerCarta.php
@@ -6,15 +6,11 @@ $cart = new Cart;
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>View Cart - PHP Shopping Cart Tutorial</title>
     <meta charset="utf-8">
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <style>
-    .container{padding: 20px;}
-    input[type="number"]{width: 20%;}
-    </style>
+
     <script>
     function updateCartItem(obj,id){
         $.get("cartAction.php", {action:"updateCartItem", id:id, qty:obj.value}, function(data){
@@ -71,7 +67,7 @@ $cart = new Cart;
             </td>
         </tr>
         <?php } }else{ ?>
-        <tr><td colspan="5"><p>Tu carta esta vacia.....</p></td>
+        <tr><td colspan="5"><p>No tienes ningun producto en el carrito</p></td>
         <?php } ?>
     </tbody>
     <tfoot>
